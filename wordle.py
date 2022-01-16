37105f962bdf291e334299b1cfda74828fcdad4e
Martin Konicek <martin@figma.com>
2022-01-16 21:01:22 +0000

    suggestion improved

--- wordle.py
+++ wordle.py
@@ -26,24 +26,19 @@ for i in range(len(allowed)):
     for c in freq.keys():
         allowed[i].add(c)
 
-def suggestion_score(word, allowed):
+def suggestion_score(word, possible):
     score = 0
-    for c in allowed:
-        if c in word:
-            score = score + 1
+    for c in word:
+        for p in possible:
+            if c in p:
+              break
+        score = score + freq[c]
     return score
 
-
-def suggestion(words, allowed):
-    all = set()
-    for a in allowed:
-        if len(a) > 1:
-            for c in a:
-                all.add(c)
-    suggestions = [(w, suggestion_score(w, all)) for w in words]
+def suggestion(words, possible):
+    suggestions = [(w, suggestion_score(w, possible)) for w in words]
     suggestions_sorted = sorted(suggestions, key=lambda t: t[1], reverse=True)
     return suggestions_sorted[:5]
-    
 
 def is_possible(word, allowed):
     for i in range(len(word)):
@@ -71,13 +66,14 @@ while True:
           # The yellow character must appear in the word
           must_appear.add(g_char)
           # But the yellow character cannot at that position
-          allowed[i].remove(g_char)
+          if g_char in allowed[i]:
+              allowed[i].remove(g_char)
                   
       if res == 'g':
           allowed[i] = { g_char }
 
   possible = find_possible(words, allowed)      
 
-  print('allowed', allowed)    
+  print('allowed', allowed)
   print(f"OK, there are now {len(possible)} possible words:", possible)
-  print(f"How about:", suggestion(words, allowed))
+  print(f"How about:", suggestion(possible, possible))
