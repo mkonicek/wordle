688fb90a1062d5e6c12cf19d8b4e6627a09d9731
Martin Konicek <martin@figma.com>
2022-01-16 20:13:41 +0000

    Improve

--- wordle.py
+++ wordle.py
@@ -15,49 +15,48 @@ def character_frequencies(words):
                 freq[char] = 1
     return freq
 
-# Characters at exact known positions. Example: [' ', 'a', ' ', ' ', 'c']
-known = [' ', ' ', ' ', ' ', ' ']
-# Chars present in the word
-present = set()
-# Chars we know never appear
-banned = set()    
+# print(words)
+# print(character_frequencies(words))
+words = load_words()
+freq = character_frequencies(words)    
 
-def is_possible(word, known, present, banned):
-    for b in banned:
-        if b in word:
-            return False
-    for p in present:
-        if not p in word:
-            return False
+allowed = [set(), set(), set(), set(), set()]
+must_appear = set()
+for i in range(len(allowed)):
+    for c in freq.keys():
+        allowed[i].add(c)
+
+def is_possible(word, allowed):
     for i in range(len(word)):
-        if (known[i] != ' ') and known[i] != word[i]:
+        if word[i] not in allowed[i]:
+            return False
+    for m in must_appear:
+        if m not in word:
             return False
     return True
 
-def find_possible(words, known, present, banned):
-    return [word for word in words if is_possible(word, known, present, banned)]
-
-# print(words)
-
-# print(character_frequencies(words))
-
-words = load_words()
-freq = character_frequencies(words)
+def find_possible(words, allowed):
+    return [word for word in words if is_possible(word, allowed)]
 
 while True:
   guess = input("What word did you guess? ")
   for i in range(len(guess)):
-      res = input(f"What was the colour of character {guess[i]} (b, y, g)? ")
+      g_char = guess[i]
+      res = input(f"What was the colour of character {g_char} (b, y, g)? ")
       if res == 'b':
-          banned.add(guess[i])
+          for j in range(len(guess)):
+              if g_char in allowed[j]:
+                  allowed[j].remove(g_char)
       if res == 'y':
-          present.add(guess[i])
+          # The yellow character must appear in the word
+          must_appear.add(g_char)
+          # But the yellow character cannot at that position
+          allowed[i].remove(g_char)
+                  
       if res == 'g':
-          known[i] = guess[i]
+          allowed[i] = { g_char }
 
-  possible = find_possible(words, known, present, banned)      
+  possible = find_possible(words, allowed)      
 
-  print('known', known)    
-  print('present', present)
-  print('banned', banned)
+  print('allowed', allowed)    
   print(f"OK, there are now {len(possible)} possible words:", possible)
